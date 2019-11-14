d871cf1e38a2f170d593d62fe4baa424564260b4
niklasbec <niklasbeckerr@gmail.com>
2019-11-14 14:16:17 +0100

    Step 1 done

--- app/src/components/Form.js
+++ app/src/components/Form.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import { Form, Field, withFormik } from 'formik';
 
-function Form() {
+function newForm() {
   return (
     <div>
-      
+      <Form>
+          <label>Name
+          <Field type='text' name='name' placeholder='Enter name here'/>
+          </label>
+
+          <label>Email
+          <Field type='email' name='email' placeholder='Enter email here'/>
+          </label>
+
+          <label>Password
+          <Field type='password' name='password' placeholder='Enter password here'/>
+          </label>
+
+          <label>TOS
+          <Field type='checkbox' name='tos' />
+          </label>
+          <input type='submit'></input>
+      </Form>
     </div>
   );
 }
 
-export default Form;
+const newFormWithFormik = withFormik ({
+    mapPropsToValues() {
+        return {
+            name: '',
+            email: '',
+            password: '',
+            tos: false
+
+        }
+    }
+})(newForm)
+
+export default newFormWithFormik;
